965a3056bdaa44b847130bbad3f613c5406b5363
Swapnil <Swapnil511994@gmail.com>
2024-09-14 16:59:57 +0530

    about us changes

--- src/Components/Landing/About.tsx
+++ src/Components/Landing/About.tsx
@@ -2,8 +2,8 @@ const About = () => {
   return (
     <div className="text-greyOpaque pl-2 pr-2">
       <h1 className="text-[52px] text-white">About Us</h1>
-      <p>
-        At Technitos, we believe that success begins with the right people. As a
+      <p className="text-white">
+        At <b>Technitos</b>, we believe that success begins with the right people. As a
         premier staffing solution provider with deep expertise in IT, we
         specialize in connecting businesses with top talent across industries.
         Whether you're looking for dynamic IT professionals, skilled developers,
@@ -20,20 +20,12 @@ const About = () => {
         ensure your business gets the support it needs to thrive.
       </p>
       <br />
-      <p>
+      <p className="text-white">
         But we don't stop at staffing. Our roots in IT excellence allow us to go
         beyond recruitment, offering insights and strategies to help you
         optimize your technology landscape and meet the demands of a rapidly
         changing digital world. At Technitos, we’re not just filling
         positions—we’re building lasting partnerships that drive results.
-      </p>
-      <br />
-      <p>
-        With years of expertise in both staffing and IT sectors, we take pride
-        in our ability to understand our clients' unique needs and deliver
-        tailored solutions. Our team of seasoned professionals is dedicated to
-        helping both employers and job seekers achieve their goals, creating a
-        win-win for everyone involved.
       </p>
       <br />
       <p>
